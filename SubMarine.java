a1cec23169f6c9ab8c37b8b467bd0a9a5b2358bb
hazki <115716929+hazki-san@users.noreply.github.com>
2022-12-27 16:01:45 +0900

    Update SubMarine.java
    submarine infを分離させました

--- SubMarine.java
+++ SubMarine.java
@@ -1,49 +1,4 @@
 import java.util.Scanner;
-class SMInf{
-  private String name;
-  private int hp;
-  private int x;
-  private int y;
-
-  SMInf(String name, int hp, int x, int y){
-    this.name = name;
-    this.hp = hp;
-    this.x = x;
-    this.y = y;
-  }
-
-  public void setName(String s){
-    name = s;
-  }
-  public void setHP(int n){
-    hp = n;
-  }
-  public void setX(int n){
-    x = n;
-  }
-  public void setY(int n){
-    y = n;
-  }
-
-  public String getName(){
-    return name;
-  }
-  public int getHP(){
-    return hp;
-  }
-  public int getX(){
-    return x;
-  }
-  public int getY(){
-    return y;
-  }
-
-  @Override
-  public String toString(){
-    return "Name: " + name + ", HP: " + hp + ", place = (" + x + ", " + y + ")\n";
-  }
-}
-
 
 class SubMarine{
   static double[][] us = new int[7][7];//0 or 1
