24f87e285882c277b112c44c56b98a2dabf37ee4
Fukuda77 <121268420+Fukuda77@users.noreply.github.com>
2023-01-03 17:28:36 +0900

    Update History.java

--- History.java
+++ History.java
@@ -1,1 +1,18 @@
+import java.util.ArrayList;
+ 
+public class MyHistory {
+  public static void main(String[] args) {
+    ArrayList<Integer> myhistory = new ArrayList<>();
+    history.add( );
+
+public class EnemyHistory{
+    ArrayList<Integer> enemyhistory = new ArrayList<>();
+    
+
+}
+
+
+    get ();
+
+}
 
