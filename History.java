397aa028242f96e40501ca907e69b0f06504b021
Fukuda77 <121268420+Fukuda77@users.noreply.github.com>
2023-01-09 00:34:11 +0900

    Update History.java

--- History.java
+++ History.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
- 
+
 public class MyHistory {
-  public static void main(String[] args) {
-    ArrayList<Integer> myhistory = new ArrayList<>();
-    history.add( );
+    public static void main(String[] args) {
+        ArrayList<Integer> myhistory = new ArrayList<>();
+        myhistory.add(ms);//msはmy action のつもりなのですがどこで定義するべきかわかりません
+    }
+}
+
 
 public class EnemyHistory{
-    ArrayList<Integer> enemyhistory = new ArrayList<>();
-    
-
+    public static void main(String[] args) {
+        ArrayList<Integer> enemyhistory = new ArrayList<>();
+        enemyhistiry.add(es); //SubMarine.javaで定義したenemy's actionを格納
+    }
 }
 
 /* スミマセンもうどうすればよいのか分からないため編集途中です。ただいま勉強中 */
 
 }
-
