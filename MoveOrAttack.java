49764c2c848d38eeb4c9d78cc482742f79b87fba
mathmaticada <121471224+mathmaticada@users.noreply.github.com>
2023-01-09 23:59:51 +0900

    Update MoveOrAttack.java

--- MoveOrAttack.java
+++ MoveOrAttack.java
@@ -1,14 +1,17 @@
 class MoveOrAttack{
   //attack
     //波高しかつHP3
-  if( && 
+  if( result == 'namitakashi' && )  
     //命中かつ次ターン以降攻撃の場合
+  if( result == 'hit' && )
   
 
   //move
     //波高しでHP2の場合
+  if( result == 'namitakashi' && )
     //命中かつHP1の場合
+  if( result == 'hit' && )
     //命中かつHP2の場合
-  if()
+  if( result == 'hit' && )
   /* どうすればよいのか分からず全く手が付けられない。今勉強してます、sorry..... */
 
