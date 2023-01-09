efac51dc5afd1d94c5290854f20587c729155869
mathmaticada <121471224+mathmaticada@users.noreply.github.com>
2023-01-10 00:09:17 +0900

    Update MoveOrAttack.java
    なんかとりあえず型的なの作ったけどあってるのか分からん

--- MoveOrAttack.java
+++ MoveOrAttack.java
@@ -1,17 +1,27 @@
 class MoveOrAttack{
   //attack
     //波高しかつHP3
-  if( result == 'namitakashi' && )  
-    //命中かつ次ターン以降攻撃の場合
-  if( result == 'hit' && )
+  if( result == 'namitakashi' && (putSub(sub1)==3 || putSub(sub2)==3 || putSub(sub3)==3 )){
+    
+  }
+     //命中かつ次ターン以降攻撃の場合
+  if( result == 'hit' && ){
+    
+  }
   
 
   //move
     //波高しでHP2の場合
-  if( result == 'namitakashi' && )
+  if( result == 'namitakashi' && (putSub(sub1)==2 || putSub(sub2)==2 || putSub(sub3)==2 )){
+    
+  }
     //命中かつHP1の場合
-  if( result == 'hit' && )
+  if( result == 'hit' && (putSub(sub1)==1 || putSub(sub2)==1 || putSub(sub3)==1 )){
+    
+  }
     //命中かつHP2の場合
-  if( result == 'hit' && )
+  if( result == 'hit' && (putSub(sub1)==2 || putSub(sub2)==2 || putSub(sub3)==2 )){
+    
+  }
   /* どうすればよいのか分からず全く手が付けられない。今勉強してます、sorry..... */
 
