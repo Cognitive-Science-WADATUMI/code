180439a8f3caa7530545d3bad6011c5d24dbc2df
mathmaticada <121471224+mathmaticada@users.noreply.github.com>
2023-01-10 00:23:26 +0900

    Update MoveOrAttack.java

--- MoveOrAttack.java
+++ MoveOrAttack.java
@@ -1,27 +1,27 @@
 class MoveOrAttack{
   //attack
     //波高しかつHP3
-  if( result == 'namitakashi' && (putSub(sub1)==3 || putSub(sub2)==3 || putSub(sub3)==3 )){
+  if( result == 'namitakashi' && (putSub(sub1)==3 || putSub(sub2)==3 || putSub(sub3)==3 || putSub(sub4)==3)){
     
   }
   
   //命中かつ次ターン以降攻撃の場合(move→attack)
-  if( result == 'hit' && (putSub(sub1)==1 || putSub(sub2)==1 || putSub(sub3)==1 )){
+  if( result == 'hit' && (putSub(sub1)==1 || putSub(sub2)==1 || putSub(sub3)==1 || putSub(sub4)==1)){
     
   }
   
 
   //move
     //波高しでHP2の場合
-  if( result == 'namitakashi' && (putSub(sub1)==2 || putSub(sub2)==2 || putSub(sub3)==2 )){
+  if( result == 'namitakashi' && (putSub(sub1)==2 || putSub(sub2)==2 || putSub(sub3)==2 || putSub(sub4)==2 )){
     
   }
     //命中かつHP1の場合
-  if( result == 'hit' && (putSub(sub1)==1 || putSub(sub2)==1 || putSub(sub3)==1 )){
+  if( result == 'hit' && (putSub(sub1)==1 || putSub(sub2)==1 || putSub(sub3)==1 || putSub(sub4)==1 )){
     
   }
     //命中かつHP2の場合
-  if( result == 'hit' && (putSub(sub1)==2 || putSub(sub2)==2 || putSub(sub3)==2 )){
+  if( result == 'hit' && (putSub(sub1)==2 || putSub(sub2)==2 || putSub(sub3)==2 || putSub(sub4)==2 )){
     
   }
   /* どうすればよいのか分からず全く手が付けられない。今勉強してます、sorry..... */
